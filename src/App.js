83d392a8af02971b2c15f3f0c7fe0005c1352bef
Tin <s3988418@rmit.edu.vn>
2025-07-07 11:48:31 +0700

    adding Navigation.js and install react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import MyAccount from "./pages/MyAccount";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navigation />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products/*" element={<Products />} />
+          <Route path="/account" element={<MyAccount />} />
+        </Routes>
+      </div>
     </div>
   );
 }
