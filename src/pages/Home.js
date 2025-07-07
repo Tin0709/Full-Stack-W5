54998e78b6a286cfad1e2a8aaced529bcc32ad02
Tin <s3988418@rmit.edu.vn>
2025-07-07 17:06:56 +0700

    better UI for Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Home.css";
+
 export default function Home() {
-  return <h2>Welcome to our store!</h2>;
+  return (
+    <div className="home-hero">
+      <div className="home-content">
+        <h1>Welcome to Our Store!</h1>
+        <p>
+          Discover great deals on high-quality products tailored just for you.
+        </p>
+        <Link to="/products" className="home-btn">
+          Browse Products
+        </Link>
+      </div>
+    </div>
+  );
 }
